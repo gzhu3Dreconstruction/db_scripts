63524628adca17f8e0fc154be44120fa11f71482
Xkein <739502808@qq.com>
2022-02-11 21:32:51 +0800

    提交建库和测试脚本

---
+++ create_database.sql
@@ -0,0 +1,52 @@
+
+CREATE DATABASE IF NOT EXISTS bridge_recon_fix;
+
+use bridge_recon_fix;
+
+DROP TABLE IF EXISTS connected_users;
+CREATE TABLE connected_users(
+    user_id INT UNSIGNED PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS current_works;
+CREATE TABLE current_works(
+    work_id INT UNSIGNED PRIMARY KEY,
+    work_progress FLOAT,
+    current_work_type INT
+);
+
+
+DROP TABLE IF EXISTS server_process_items;
+CREATE TABLE server_process_items(
+    item_id INT UNSIGNED AUTO_INCREMENT,
+    work_id INT UNSIGNED,
+    item_data VARCHAR(2048) NOT NULL,
+    process_type INT,
+    PRIMARY KEY(item_id, work_id),
+    FOREIGN KEY(work_id) REFERENCES current_works(work_id)
+);
+
+--0 号工作用于请求新的工作
+INSERT INTO current_works (work_id, work_progress, current_work_type) VALUES (0, 0, 0);
+
+DROP TABLE IF EXISTS client_process_items;
+CREATE TABLE client_process_items(
+    item_id INT UNSIGNED AUTO_INCREMENT,
+    work_id INT UNSIGNED,
+    item_data VARCHAR(2048) NOT NULL,
+    process_type INT,
+    PRIMARY KEY(item_id, work_id),
+    FOREIGN KEY(work_id) REFERENCES current_works(work_id)
+);
+
+-- 创建服务器用户
+CREATE USER 'recon3d_server'@'%' IDENTIFIED BY 'recon3d';
+GRANT ALL ON bridge_recon_fix.* TO 'recon3d_server'@'%' WITH GRANT OPTION;
+-- 创建客户端用户
+CREATE USER 'recon3d_client'@'%' IDENTIFIED BY '666666';
+GRANT SELECT,INSERT ON bridge_recon_fix.server_process_items TO 'recon3d_client'@'%';
+GRANT SELECT ON bridge_recon_fix.client_process_items TO 'recon3d_client'@'%';
+GRANT SELECT ON bridge_recon_fix.current_works TO 'recon3d_client'@'%';
+GRANT SELECT ON bridge_recon_fix.connected_users TO 'recon3d_client'@'%';
+
+
---
+++ test.sql
@@ -0,0 +1,20 @@
+
+use bridge_recon_fix;
+
+INSERT INTO connected_users (user_id) VALUES (1);
+DELETE FROM connected_users WHERE user_id=1;
+
+INSERT INTO current_works (work_id, work_progress, current_work_type) VALUES (114514, 0, 0);
+SELECT COUNT(*) FROM current_works;
+SELECT * FROM current_works;
+UPDATE current_works SET work_progress=0.5, current_work_type=1 WHERE work_id=114514;
+DELETE FROM current_works WHERE work_id=114514;
+
+INSERT INTO server_process_items (work_id, item_data, process_type) VALUES (1919810, '/xkein/home/recon/server/tmp', 0);
+DELETE FROM server_process_items WHERE work_id=114514;
+SELECT * FROM server_process_items;
+
+-- send work request
+INSERT INTO server_process_items (work_id, item_data, process_type) VALUES (0, 'request token!', 1);
+-- send work finish
+INSERT INTO server_process_items (work_id, item_data, process_type) VALUES (958169357, 'request token!', 2);
